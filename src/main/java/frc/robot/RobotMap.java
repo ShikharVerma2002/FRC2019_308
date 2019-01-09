ac5187d5c2ac5ba56fc3b1306d5b07453f3dd051
MatteoMoceri <matteomoceri@gmail.com>
2019-01-09 16:41:22 -0500

    Initial commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,77 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.*;
+
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+  //Motors
+    //Drive Motors
+  public static WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(0);
+  public static WPI_TalonSRX rightFrontMotor = new WPI_TalonSRX(1);
+  public static WPI_TalonSRX leftBackMotor = new WPI_TalonSRX(2);
+  public static WPI_TalonSRX rightBackMotor = new WPI_TalonSRX(3);
+
+    //Intake Motors
+
+    //Delivery mechanism motors
+
+  //Sensors
+    //Chassis sensors
+  public static Gyro chassisGyro = new Gyro(){
+  
+    @Override
+    public void reset() {
+      
+    }
+  
+    @Override
+    public double getRate() {
+      return 0;
+    }
+  
+    @Override
+    public double getAngle() {
+      return 0;
+    }
+  
+    @Override
+    public void free() {
+      
+    }
+  
+    @Override
+    public void calibrate() {
+      
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+  };
+
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+}
